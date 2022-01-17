8dcade5629c68a9fd58407d022ddb0b638d792b3
MUM-TSI-AN-L362 <MUM-TSI-AN-L362@MUM-TSI-AN-L362>
2022-01-17 13:28:32 +0530

    First Push

---
+++ src/test/java/Gproject/newdemoproject.java
@@ -0,0 +1,11 @@
+package Gproject;
+
+import org.testng.annotations.Test;
+
+public class newdemoproject {
+	@Test
+	public void tester() {
+		System.out.println("--------automation---------");
+	}
+
+}
